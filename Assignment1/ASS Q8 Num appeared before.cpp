1cc0bac28743fbcbcdd9aaa937f7b12e8d6e13e6
Sanchit Sehgal <sanchitsehgal16@gmail.com>
2025-09-03 16:37:11 +0530

    intal commit

---
+++ Assignment1/ASS Q8 Num appeared before.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter " << n << " elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int distinctCount = 0;
+
+    for (int i = 0; i < n; i++) {
+        bool isDuplicate = false;
+        // check if arr[i] appeared before
+        for (int j = 0; j < i; j++) {
+            if (arr[i] == arr[j]) {
+                isDuplicate = true;
+                break;
+            }
+        }
+        if (!isDuplicate) {
+            distinctCount++;
+        }
+    }
+
+    cout << "Total distinct elements: " << distinctCount << endl;
+
+    return 0;
+}
