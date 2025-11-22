235c312dd31131fde78cab9cf90c1c6d3a701a7c
Sanchit Sehgal <sanchitsehgal16@gmail.com>
2025-11-22 18:05:34 +0530

    inital commit

--- Assignment1/ASS Q2 Bubble sort.cpp
+++ Assignment1/ASS Q2 Bubble sort.cpp
@@ -10,18 +10,18 @@ void bubbleSort(vector<int>& arr) {
         swapped = false;
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // swap arr[j] and arr[j+1]
+                
                 swap(arr[j], arr[j + 1]);
                 swapped = true;
             }
         }
-        // If no elements were swapped, array is already sorted
+        
         if (!swapped) break;
     }
 }
 
 int main() {
-    // Dynamic array using vector
+    
     vector<int> arr = {64, 34, 25, 12, 22, 11, 90};
 
     cout << "Unsorted Array: ";
--- Assignment1/ASS Q3 linear binary comparision.cpp
+++ Assignment1/ASS Q3 linear binary comparision.cpp
@@ -2,37 +2,32 @@
 #include <vector>
 using namespace std;
 
-// (a) Linear Search O(n)
 int findMissingLinear(const vector<int>& arr, int n) {
     for (int i = 0; i < arr.size(); i++) {
         if (arr[i] != i + 1) {
-            return i + 1; // missing number
+            return i + 1;
         }
     }
-    return n; // if last number is missing
+    return n;
 }
 
-// (b) Binary Search O(log n)
 int findMissingBinary(const vector<int>& arr, int n) {
     int left = 0, right = arr.size() - 1;
 
     while (left <= right) {
         int mid = (left + right) / 2;
 
-        // If value matches its index+1, missing is on the right
         if (arr[mid] == mid + 1) {
             left = mid + 1;
         }
-        // Otherwise missing number is on the left
         else {
             right = mid - 1;
         }
     }
-    return left + 1; // missing number
+    return left + 1;
 }
 
 int main() {
-    // Example: n = 7, numbers should be 1 to 7 but 5 is missing
     vector<int> arr = {1, 2, 3, 4, 6, 7};
     int n = 7;
 
